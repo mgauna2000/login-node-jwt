468c5799a643ae099c68e5da27e38fe4169c8291
Martin <martingauna2000@gmail.com>
2022-01-10 23:28:03 -0300

    config structure

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const cookieParser = require('cookie-parser')
 const app = express()
 //seteamos el motor de plantilla
 app.set('view engine', 'ejs')
+//seteamos la carpeta public para archivos estaticos
+app.use(express.static('public'))
+//para procesar datos enviados desde form
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+//seteamos las variables de entorno
+dotenv.config({path: './env/.env'})
+//seteamos las cookies
+app.use(cookieParser)
 
 app.get('/', (req, res) => {
     res.send('Hola mundo')
