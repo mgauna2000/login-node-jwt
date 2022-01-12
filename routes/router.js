7ed1fe71e5e2fcde5c05149f9d08bebce8cff3e8
Martin <martingauna2000@gmail.com>
2022-01-11 21:02:31 -0300

    connecting database

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('../database/db')
 
 router.get('/', (req, res) => {
+    connection()
     res.render('index')
 })
 router.get('/login', (req, res) => {
