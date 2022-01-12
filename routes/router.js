062a29c7db69f016071eaa519c9f37c8a4071067
Martin <martingauna2000@gmail.com>
2022-01-11 21:53:32 -0300

    add bootstrap pages

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const connection = require('../database/db')
 
 router.get('/', (req, res) => {
-    connection()
     res.render('index')
 })
 router.get('/login', (req, res) => {
